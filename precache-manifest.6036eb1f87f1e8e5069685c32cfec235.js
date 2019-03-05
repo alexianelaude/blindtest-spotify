6b7f9fdb97822eecc2e72f28dd141d306582c761
Alexiane <alexiane.laude@mines-paristech.fr>
2019-03-05 19:52:23 +0100

    Updates

---
+++ precache-manifest.6036eb1f87f1e8e5069685c32cfec235.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "79f6b6a0c4f3cca321dc458fe7e6544a",
+    "url": "/blindtest-spotify/static/media/logo.79f6b6a0.svg"
+  },
+  {
+    "revision": "9d4cbf40dcdac09bda6fa0aff397246b",
+    "url": "/blindtest-spotify/static/media/loading.9d4cbf40.svg"
+  },
+  {
+    "revision": "bd4de7ae32d74b318af5",
+    "url": "/blindtest-spotify/static/js/runtime~main.bd4de7ae.js"
+  },
+  {
+    "revision": "8e5af351f5c790980e33",
+    "url": "/blindtest-spotify/static/js/main.8e5af351.chunk.js"
+  },
+  {
+    "revision": "3f7525847cbf7fd2fe0c",
+    "url": "/blindtest-spotify/static/js/1.3f752584.chunk.js"
+  },
+  {
+    "revision": "8e5af351f5c790980e33",
+    "url": "/blindtest-spotify/static/css/main.19971ac6.chunk.css"
+  },
+  {
+    "revision": "745dff2a7a8d302150287f0b2676fa16",
+    "url": "/blindtest-spotify/index.html"
+  }
+];
